f12c288ffc0a71c46c5c1a43a5f60c368a8faea4
Nitesh47kumars <niteshshah028@gmail.com>
2025-09-19 19:56:28 +0530

    Add name with Logo in Navbar

--- src/Components/Navbar/NavLogo.jsx
+++ src/Components/Navbar/NavLogo.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 const NavLogo = () => {
   return (
-    <div className="h-10 w-10">
+    <div className='flex items-center justify-center gap-2'>
+      <div className="h-10 w-10">
         <img
-        src='Logo.png'
-        alt='logo'
-        className='h-full w-full'/>
+          src='Logo.png'
+          alt='logo'
+          className='h-full w-full'/>
+        </div>
+        <h1 className='text-2xl font-bold uppercase'>Brand</h1>
     </div>
   )
 }
