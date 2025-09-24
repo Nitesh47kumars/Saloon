3d22ebcae31834870d43499fe722544c77fcb9c4
Nitesh47kumars <niteshshah028@gmail.com>
2025-09-24 21:08:33 +0530

    Fix Margin & Padding of Services Component

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -5,12 +5,12 @@ import services from './Services.json'
 const Services = () => {
   return (
     <section id="services" className="bg-gray-50 px-6 mt-25 md:mt-40">
-      <div className="w-[90%] xl:w-[80%] mx-auto py-20 md:py-25">
+      <div className="w-[90%] xl:w-[80%] mx-auto py-20">
         <h2 className="text-4xl font-extrabold text-center text-pink-600 uppercase">
           Our Services
         </h2>
 
-        <div className="space-y-30 mt-20 md:mt-25">
+        <div className="space-y-30 mt-20 md:my-20">
           {services.map((service, index) => (
             <ServiceItem
               key={index}
